3a5585682a6ffc61229b017cd8722c17ba75feb9
NazarYarkulov <120525226+Nazar-Affekta@users.noreply.github.com>
2023-07-14 20:11:16 +0300

    Initial

---
+++ conditionsPractice/ConditionPracticeClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace conditionsPractice
+{
+    public class ConditionPracticeClass
+    {
+        /*TODO: The method must receive one parameter of the int type as input - a number to check.
+        The method must return a boolean value, depending on whether the input parameter is even or odd*/
+        public static bool IsEven(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Receives a string as input and returns true if the string contains only unique characters, false otherwise. */
+        public static bool HasUniqueCharacters(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Receives an integer as input and returns true if the number is divisible by both 5 and 7, excluding zero, false otherwise. */
+        public static bool IsDivisibleBy5And7(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Performs the specified arithmetic operation (char input paramether) on two double input paramethers and returns the result. */
+        public static double PerformOperation(char operation, double number1, double number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Receives an integer as input and returns "Positive" if the number is positive, "Negative" if the number is negative, "Zero" if the number is zero. */
+        public static string CheckNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Receives a string as input and returns true if the string is a palindrome, false otherwise. */
+        public static bool IsPalindrome(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Receives a int year as input paramether and returns true if the year is a leap year, false otherwise. */
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /* TODO: Receives a character as input and returns true if the character is a vowel, false otherwise. */
+        public static bool IsVowel(char character)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Converts a binary number represented as a string to its integer equivalent. */
+        public static int BinaryToDecimal(string binaryNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* TODO: Converts a binary number represented as a string to its hexadecimal string equivalent. */
+        public static string BinaryToHexadecimal(string binaryNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
